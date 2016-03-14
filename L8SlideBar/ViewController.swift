7eefc13244555d63a9086dd128057aa9c579b62c
Chuanxun <chuanxun@chuanxuntekimac-mini.local>
2016-03-14 14:48:02 +0800

    Initial Commit

---
+++ L8SlideBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  L8SlideBar
+//
+//  Created by Chuanxun on 16/3/14.
+//  Copyright © 2016年 Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
